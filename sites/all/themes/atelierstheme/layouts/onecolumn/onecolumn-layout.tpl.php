4a2d4bca3119f92b74ee542e1463133c2c90f079
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-03-31 10:18:28 +0200

    wip modifs header

--- sites/all/themes/atelierstheme/layouts/onecolumn/onecolumn-layout.tpl.php
+++ sites/all/themes/atelierstheme/layouts/onecolumn/onecolumn-layout.tpl.php
@@ -1,24 +1,23 @@
   <?php $theme = drupal_get_path("theme",$GLOBALS['theme']) ;?>
-   
-
    <div <?php print $attributes;?>>
     <header class="l-header">
-       <?php print $variables["ateliers_theme"]; ?>
-        <img class="header-img" src="<?php print $variables["ateliers_theme"]; ?>/images/u44.png" alt="header" title="header" />
-        Test de test de test
         <div class="burger h">
             <svg x="0px" y="0px" viewBox="0 0 108.15 65.812498" width="100%" height="100%">
                 <g id="Calque_1" transform="matrix(2,0,0,2,-0.6,-2.2500009)" style="stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none">
-                    <path id="XMLID_895_" class="st0" d="m 54.2,27.3 v -13 L 43,7.8 31.7,14.3 v 13 L 43,33.8 54.2,27.3 z" inkscape:connector-curvature="0" style="fill:none;stroke:#c0b396;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
                     <g id="g8" style="stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none">
-                        <g id="g10" style="stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none">
-                            <rect x="37.099998" y="20.4" id="burger_1" class="st1" width="11.8" height="0.80000001" id="rect18" style="fill:#c0b396;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
-                            <rect x="37.099998" y="16.5" id="burger_2" class="st1" width="11.8" height="0.80000001" id="rect20" style="fill:#c0b396;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
-                            <rect x="37.099998" y="24.4" id="burger_3" class="st1" width="11.8" height="0.80000001" id="rect22" style="fill:#c0b396;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
-                        </g>
+                        <rect x="37.099998" y="20.4" id="burger_1" class="st1" width="11.8" height="0.80000001" id="rect18" style="fill:black;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
+                        <rect x="37.099998" y="16.5" id="burger_2" class="st1" width="11.8" height="0.80000001" id="rect20" style="fill:black;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
+                        <rect x="37.099998" y="24.4" id="burger_3" class="st1" width="11.8" height="0.80000001" id="rect22" style="fill:black;stroke-width:0.74999999;stroke-miterlimit:4;stroke-dasharray:none" />
                     </g>
                 </g>
             </svg>
+        </div>
+        <img class="header-img" src="<?php print $theme; ?>/images/u44.png" alt="header" title="header" />
+        <img class="logo-img" src="<?php print $theme; ?>/images/logo.png" alt="logo" title="logo" />
+        <div class="block-w">
+            
+            <h2>Bienvenue aux ateliers de l'ilôt </h2>
+            
         </div>
         <?php print render($page['header']); ?>
     </header>
