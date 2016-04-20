e2ee8e6e2b152566b66adee1b01f5917f9324567
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-04-20 18:01:45 +0200

    wip mise en page mentions légales

--- sites/all/themes/atelierstheme/templates/node/node--basic-page.tpl.php
+++ sites/all/themes/atelierstheme/templates/node/node--basic-page.tpl.php
@@ -1,12 +1,17 @@
 <?php
 
-    $body = field_view_field("node",$node,'field_body');
+    $body = field_view_field("node",$node,'body');
 
 ?>
+
+COUCOU
+
 <div class="mentions-legales">
 
         <div class="body-mentions-legales">
             <?php print render($body); ?>
         </div>
+
+        <p> COUCOULL </p>
 
 </div>
