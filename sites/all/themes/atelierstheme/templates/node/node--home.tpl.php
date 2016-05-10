581b7d93e8fd6c5762f89fc3f1ef3be6b56d2eb2
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-05-10 11:54:13 +0200

    wip de wip

--- sites/all/themes/atelierstheme/templates/node/node--home.tpl.php
+++ sites/all/themes/atelierstheme/templates/node/node--home.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
     $styles  = drupal_get_css();
     $body_actu = field_view_field("node",$node,'field_body_actu');
     $body = field_view_field("node",$node,'field_body');
@@ -56,113 +57,114 @@
     }
     //dpm($idti);
 ?>
+
 <div class="home">
-        <div class="block-all swiper-container">
-            <div class="swiper-wrapper">
-                <?php for($i = 0; $i < count($idti); $i++){
-                    $collectiong = $collectionsg[$idti[$i]];
-                    $image_field = field_get_items('field_collection_item', $collectiong, 'field_imge_presentation');
-                    $image_render = field_view_value('field_collection_item', $collectiong, 'field_imge_presentation', $image_field[0], array(
-                        'type' => 'image',
-                        'settings' => array(
-                            'image_style' => 'header',
-                        )));
-                    $image_render_mobile = field_view_value('field_collection_item', $collectiong, 'field_imge_presentation', $image_field[0], array(
+    <div class="block-all swiper-container">
+        <div class="swiper-wrapper">
+            <?php for($i = 0; $i < count($idti); $i++){
+                $collectiong = $collectionsg[$idti[$i]];
+                $image_field = field_get_items('field_collection_item', $collectiong, 'field_imge_presentation');
+                $image_render = field_view_value('field_collection_item', $collectiong, 'field_imge_presentation', $image_field[0], array(
+                    'type' => 'image',
+                    'settings' => array(
+                        'image_style' => 'header',
+                    )));
+                $image_render_mobile = field_view_value('field_collection_item', $collectiong, 'field_imge_presentation', $image_field[0], array(
+                    'type' => 'image',
+                    'settings' => array(
+                        'image_style' => 'slider',
+                    )));
+                $titre = field_view_field('field_collection_item', $collectiong, 'field_titre');
+                $chapeau = field_view_field('field_collection_item', $collectiong, 'field_body_presentation');
+            ?>
+            <div class="home swiper-slide">
+                <div class="block-img" >
+                   <?php print render($image_render); ?>
+                </div>
+                <div class="block-w">
+                    <h2 class="title-bienvenue"><?php print render($titre); ?></h2>
+                    <div class="par-bienvenue">
+                        <?php print render($chapeau); ?>
+                    </div><!--
+                    --><div class="ateliers">
+                    <!--
+                    <?php
+                    foreach($result_ateliers as $key => $value) { ?>
+                        --><div class="atelier">
+                        <?php
+                          $n = node_load($value->nid);
+                          $link =drupal_get_path_alias("node/".$value->nid);
+                          $picto = field_view_field("node",$n,'field_picto');
+                          $title = field_view_field("node",$n,'field_title');
+                          $class = field_view_field("node",$n,'field_class');
+                          $title_render = render($title);
+                          $class_render = render($class);
+                          $picto_render = render($picto);
+                          print l($picto_render, $link,array("html"=>true, 'attributes' => array('class' => array('picto_ateliers', $class_render))));
+                          $title_render = render($title);
+                          echo "<p class=\"title-pictos ".$class_render."\">";
+                            print l($title_render, $link,array("html"=>true, 'attributes' => array('class' => array('picto_ateliers'))));
+                            echo "</p>";
+                          // print l($picto_render,$link,array("html"=>true, 'attributes' => array('class' => array('plus'))));
+                        ?>
+                        </div><!--
+                     <?php } ?>
+                      -->
+                    </div>
+                </div>
+            </div>
+            <?php }  ?>
+        </div>
+    </div>
+</div>
+<div class="home-body-actu swiper-container">
+    <div class="swiper-wrapper">
+        <?php foreach($result as $key => $value) { ?>
+           <div class="home-body-padding actu-gallerie-images swiper-slide">
+               <?php
+                    $n = node_load($value->nid);
+                    $im_offre = field_get_items("node",$n,'field_image');
+                    $im_offre_r = field_view_value('node', $n, 'field_image', $im_offre[0], array(
                         'type' => 'image',
                         'settings' => array(
                             'image_style' => 'slider',
-                        )));
-                    $titre = field_view_field('field_collection_item', $collectiong, 'field_titre');
-                    $chapeau = field_view_field('field_collection_item', $collectiong, 'field_body_presentation');
-                ?>
-                <div class="home swiper-slide">
-                    <div class="block-img" >
-                       <?php print render($image_render); ?>
-                    </div>
-                    <div class="block-w">
-                        <h2 class="title-bienvenue"><?php print render($titre); ?></h2>
-                        <div class="par-bienvenue">
-                            <?php print render($chapeau); ?>
-                        </div><!--
-                        --><div class="ateliers">
-                        <!--
-                        <?php
-                        foreach($result_ateliers as $key => $value) { ?>
-                            --><div class="atelier">
-                            <?php
-                              $n = node_load($value->nid);
-                              $link =drupal_get_path_alias("node/".$value->nid);
-                              $picto = field_view_field("node",$n,'field_picto');
-                              $title = field_view_field("node",$n,'field_title');
-                              $class = field_view_field("node",$n,'field_class');
-                              $title_render = render($title);
-                              $class_render = render($class);
-                              $picto_render = render($picto);
-                              print l($picto_render, $link,array("html"=>true, 'attributes' => array('class' => array('picto_ateliers', $class_render))));
-                              $title_render = render($title);
-                              echo "<p class=\"title-pictos ".$class_render."\">";
-                                print l($title_render, $link,array("html"=>true, 'attributes' => array('class' => array('picto_ateliers'))));
-                                echo "</p>";
-                              // print l($picto_render,$link,array("html"=>true, 'attributes' => array('class' => array('plus'))));
-                            ?>
-                            </div><!--
-                         <?php } ?>
-                          -->
-                        </div>
-                    </div>
+                        ),
+                    ));
+                    $title_offres = field_view_field("node",$n,'field_title');
+                    $body_offres = field_view_field("node",$n,'field_chapeau');
+               ?>
+                <div class="im_offres">
+                    <?php print render($im_offre_r); ?>
+                </div><!--
+               --><div class="body_offres ">
+                    <?php
+                        echo "<h1>".render($title_offres)."</h1>";
+                        print render($body_offres);
+                    ?>
+                     <div class="center">
+                      <?php
+                        print l("En savoir plus", $link,array("html"=>true, 'attributes' => array('class' => array('en_savoir_plus'))));
+                       ?>
+                      </div>
                 </div>
-                <?php }  ?>
             </div>
+        <?php  } ?>
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+</div>
+<div class="home-body-solidarite home-body-padding ">
+    <div class="image_solidarite">
+        <?php print render($image); ?>
+    </div>
+    <div class="body_solidarite">
+        <?php print render($body); ?>
+        <div class="center">
+            <?php //$theme = base_path() . drupal_get_path("theme",$GLOBALS['theme']) ;
+                print l("En savoir plus", "/content/présentation", array("html"=>true, 'attributes' => array('class' => array('en_savoir_plus'))));
+            ?>
         </div>
     </div>
-    <div class="home-body-actu swiper-container">
-        <div class="swiper-wrapper">
-            <?php foreach($result as $key => $value) { ?>
-               <div class="home-body-padding actu-gallerie-images swiper-slide">
-                   <?php
-                        $n = node_load($value->nid);
-                        $im_offre = field_get_items("node",$n,'field_image');
-                        $im_offre_r = field_view_value('node', $n, 'field_image', $im_offre[0], array(
-                            'type' => 'image',
-                            'settings' => array(
-                                'image_style' => 'slider',
-                            ),
-                        ));
-                        $title_offres = field_view_field("node",$n,'field_title');
-                        $body_offres = field_view_field("node",$n,'field_chapeau');
-                   ?>
-                    <div class="im_offres">
-                        <?php print render($im_offre_r); ?>
-                    </div><!--
-                   --><div class="body_offres ">
-                        <?php
-                            echo "<h1>".render($title_offres)."</h1>";
-                            print render($body_offres);
-                        ?>
-                         <div class="center">
-                          <?php $link =drupal_get_path_alias("node/".$value->nid);
-                            print l("En savoir plus", $link,array("html"=>true, 'attributes' => array('class' => array('en_savoir_plus'))));
-                           ?>
-                          </div>
-                    </div>
-                </div>
-            <?php  } ?>
-        </div>
-        <div class="swiper-pagination"></div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-    </div>
-    <div class="home-body-solidarite home-body-padding ">
-        <div class="image_solidarite">
-            <?php print render($image); ?>
-        </div>
-        <div class="body_solidarite">
-            <?php print render($body); ?>
-            <div class="center">
-                <?php $link_auto_promo =drupal_get_path_alias("node/".$auto_promo[0]["entity"]->nid);
-                    print l("En savoir plus", $link_auto_promo,array("html"=>true, 'attributes' => array('class' => array('en_savoir_plus'))));
-                ?>
-            </div>
-        </div>
-        <div class="clear"></div>
-    </div>
+    <div class="clear"></div>
+</div>
