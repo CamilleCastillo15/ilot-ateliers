6f29e3d3d04d8386004204be98bd7ed8e05fa99f
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-06-01 10:05:47 +0200

    wip : le bloc adresse est modifiable dans le back - office

--- sites/all/modules/ateliersmod/templates/ateliersmod_address.tpl.php
+++ sites/all/modules/ateliersmod/templates/ateliersmod_address.tpl.php
@@ -14,10 +14,36 @@
         <?php foreach($result_adresses as $key => $value) { ?>
 
         <?php $n = node_load($value->nid);
-                      $link =drupal_get_path_alias("node/".$value->nid);
-                      $texte = field_view_field("node",$n,'body'); ?>
+              $link =drupal_get_path_alias("node/".$value->nid);
+              $titre = field_view_field("node",$n,'field_titre');
+              $horaires = field_view_field("node",$n,'field_texte_horaires');
+              $adresse = field_view_field("node",$n,'field_texte_adresse');
+              $picto_tel = field_view_field("node",$n,'field_image');
+              $telephone = field_view_field("node",$n,'field_texte_telephone');
+              $emails = field_view_field("node",$n,'field_texte_emails');
+         ?>
+                  <h1>
+                      <?php print render($titre); ?>
+                  </h1>
+                  <br />
+                  <div class="horaires">
+                      <?php print render($horaires); ?>
+                  </div>
+                  <br />
+                  <div class="adresse">
+                      <?php print render($adresse); ?>
+                  </div>
+                  <br />
+                    <?php print render($picto_tel); ?>
+                  <div class="telephone">
+                      <?php print render($telephone); ?>
+                  </div>
+                  <br />
+                  <br />
+                  <div class="emails">
+                      <?php print render($emails); ?>
+                  </div>
 
-                      <?php print render($texte); ?>
        <?php } ?>
 
 </div>
